2319fe1ee657c53deb9cefd85debc0d32a51464f
Gnaarf <gnaarf@gmx.de>
2018-05-29 22:56:04 +0200

    implemented siftFeature extraction

--- ImageUnderstanding/Code/Classifier/MyClassifier.cs
+++ ImageUnderstanding/Code/Classifier/MyClassifier.cs
@@ -15,20 +15,85 @@ namespace ImageUnderstanding.Classifier
         {
             sift = new SIFT();
         }
-        
+
         public override void Train(List<TaggedImage> trainingDataSet)
         {
-            if(trainingDataSet.Count > 0)
+            foreach (TaggedImage image in trainingDataSet)
             {
-                TaggedImage image = trainingDataSet[0];
+                List<float> featureVector = ExtractSiftFeatureVector(image, 20, SiftSorting.Response, true);
 
-                Emgu.CV.Util.VectorOfKeyPoint vectorOfKeypoints = new Emgu.CV.Util.VectorOfKeyPoint();
+            }
 
-                Mat output = image.GetMat().Clone();
+            return;
+        }
 
-                sift.DetectAndCompute(image.GetMat(), null, vectorOfKeypoints, output, false);
+        public override string Evaluate(TaggedImage dataSample)
+        {
+            throw new NotImplementedException();
+        }
 
-                Features2DToolbox.DrawKeypoints(image.GetMat(), vectorOfKeypoints, output, new Bgr(0, 0, 0));
+        enum SiftSorting
+        {
+            Response,
+            Size,
+            None,
+        }
+
+        List<float> ExtractSiftFeatureVector(TaggedImage image, int keyPointCount, SiftSorting sorting, bool doDrawEverySingleImage)
+        {
+            // use the emgu functions to gather keypoints
+
+            Emgu.CV.Util.VectorOfKeyPoint vectorOfKeypoints = new Emgu.CV.Util.VectorOfKeyPoint();
+
+            Mat output = image.GetMat().Clone(); // only needed for drawing
+
+            sift.DetectAndCompute(image.GetMat(), null, vectorOfKeypoints, output, false);
+
+            // put it into useful data formats
+
+            List<MKeyPoint> keyPoints = new List<MKeyPoint>(vectorOfKeypoints.ToArray());
+
+            // sort
+
+            switch (sorting)
+            {
+                case SiftSorting.Response:
+                    keyPoints.Sort((p1, p2) => p1.Response < p2.Response ? 1 : (p1.Response == p2.Response ? 0 : -1));
+                    break;
+
+                case SiftSorting.Size:
+                    keyPoints.Sort((p1, p2) => p1.Size < p2.Size ? 1 : (p1.Size == p2.Size ? 0 : -1));
+                    break;
+
+                case SiftSorting.None:
+                default:
+                    break;
+            }
+
+            // trim
+
+            keyPoints.RemoveRange(keyPointCount, keyPoints.Count - keyPointCount);
+
+            // convert into list
+
+            List<float> result = new List<float>(keyPointCount * 4);
+            for (int i = 0; i < keyPointCount; ++i)
+            {
+                MKeyPoint current = keyPoints[i];
+
+                result.Add(current.Point.X);
+                result.Add(current.Point.Y);
+                result.Add(current.Size);
+                result.Add(current.Angle);
+            }
+
+            // visualize
+
+            if (doDrawEverySingleImage)
+            {
+                vectorOfKeypoints = new Emgu.CV.Util.VectorOfKeyPoint(keyPoints.ToArray());
+
+                Features2DToolbox.DrawKeypoints(image.GetMat(), vectorOfKeypoints, output, new Bgr(0, 0, 255), Features2DToolbox.KeypointDrawType.DrawRichKeypoints);
 
 
                 String win1 = "SIFT"; //The name of the window
@@ -39,24 +104,7 @@ namespace ImageUnderstanding.Classifier
                 CvInvoke.DestroyWindow(win1); //Destroy the window if key is pressed
             }
 
-            return;
-
-            foreach(TaggedImage image in trainingDataSet)
-            {
-
-                Emgu.CV.Util.VectorOfKeyPoint vectorOfKeypoints = new Emgu.CV.Util.VectorOfKeyPoint();
-                
-                Mat output = image.GetMat().Clone();
-
-                sift.DetectAndCompute(image.GetMat(), null, vectorOfKeypoints, output, false);
-            }
-
-            throw new NotImplementedException();
-        }
-
-        public override string Evaluate(TaggedImage dataSample)
-        {
-            throw new NotImplementedException();
+            return result;
         }
     }
 }
