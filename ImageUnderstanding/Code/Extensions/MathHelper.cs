e4aebb56f0084affdd5dad974b5931d1183fbf28
Gnaarf <gnaarf@gmx.de>
2018-06-22 19:57:59 +0200

    Feature Vector Normalization

---
+++ ImageUnderstanding/Code/Extensions/MathHelper.cs
@@ -0,0 +1,39 @@
+﻿using Emgu.CV.CvEnum;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Emgu.CV
+{
+    public static class FloatArrayExtension
+    {
+        public static float Mean(this float[] values)
+        {
+            float result = 0F;
+            for(int i = 0; i < values.Length; ++i)
+            {
+                result += values[i];
+            }
+            return result / (float)values.Length;
+        }
+
+        public static float StdDeviation(this float[] values)
+        {
+            return values.StdDeviation(values.Mean());
+        }
+
+        public static float StdDeviation(this float[] values, float mean)
+        {
+            float result = 0F;
+
+            for (int i = 0; i < values.Length; ++i)
+            {
+                float f = values[i] - mean;
+                result += (f * f) / (float)(values.Length - 1);
+            }
+
+            return (float)Math.Sqrt(result);
+        }
+    }
+}
